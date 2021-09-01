994add9be7cf71b82cf87e4181499b4f57b83831
Garrett Thor <garrettthor@gmail.com>
2021-09-01 08:57:06 -0700

    fixed .gitignore.  reseting for the morning bc computer no likey me

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('js connected')
+console.log('js trabajando guey');
+
+const expandResoBtn = document.querySelector('#expand-reso');
+ 
+expandResoBtn.addEventListener('click', expandReso);
+
+function expandReso() {
+    alert('Expand reso btn pushed.')
+}
